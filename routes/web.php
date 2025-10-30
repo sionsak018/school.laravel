be9f25219a0a9c76e6a0d8e64b76f162e5aebca6
kongsun855 <unkongsun23@gmail.com>
2025-10-30 15:56:06 +0700

    sun

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';*/
 
 use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', [PageController::class, 'index']);
 Route::get('/about', [PageController::class, 'about']);
